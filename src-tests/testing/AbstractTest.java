fb69848507ddfbc120d8c1d0a26b3417912290b5
krajes <krajesplock@gmail.com>
2017-06-17 10:56:55 +0200

    Dodano annotację Ignore do klasy AbstractTest.

--- src-tests/testing/AbstractTest.java
+++ src-tests/testing/AbstractTest.java
@@ -1,7 +1,9 @@
 package testing;
 
 import static org.junit.Assert.*;
+import org.junit.Ignore;
 
+@Ignore
 public abstract class AbstractTest {
     public void failException(Throwable throwable) {
         fail("Nieoczekiwany wyjatek: " + throwable.getClass());
